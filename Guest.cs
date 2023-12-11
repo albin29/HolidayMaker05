4bcc9a903f5fd41bb3ca649949f714ccfe1a8638
DanijelGb <124356150+DanijelGb@users.noreply.github.com>
2023-12-11 14:22:29 +0100

    added guest

---
+++ Guest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayMaker05;
+
+public record Guest(string name, string lastName, string email, string date, int phoneNumber)
+{
+
+
+
+
+
+
+
+}
